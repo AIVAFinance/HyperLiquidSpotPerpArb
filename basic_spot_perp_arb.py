c99d14734904665df801dc1bcffee5750c2d5545
CryingBaby <apple@AppledeMacBook-Pro.local>
2025-01-31 22:18:55 +0800

    Add Logging Mechanism and a Log file; Fix a bug checking neg funding rate and close pos.

--- basic_spot_perp_arb.py
+++ basic_spot_perp_arb.py
@@ -1,4 +1,5 @@
 from hyperliquid.utils import constants
+import logging
 import time
 import threading
 from datetime import datetime
@@ -33,10 +34,30 @@ class HypeSpotPerpArbitrage:
 
         self.perp_max_decimals = 6
         self.spot_max_decimals = 8
+        
+        self.logger = None
+        self.setup_logger()
+        self.logger.info("Initializing Arbitrage Strategy...")
+        self.logger.info(f"Trading pair: {self.pair}")
 
         # The following two attributes are deprecated as is the function check_position_value
         self.initial_position_value = None
         self.position_value_safe_percentage = 0.4
+    
+    def setup_logger(self):
+        """Setup logger to log messages to both console and a log file."""
+        log_filename = "arbitrage.log"
+        
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            handlers=[
+                logging.FileHandler(log_filename, mode='a'),  # Append logs to file
+                logging.StreamHandler()  # Print logs to console
+            ]
+        )
+        
+        self.logger = logging.getLogger(__name__)
     
     def _check_perp_open(self):
         """
@@ -54,6 +75,7 @@ class HypeSpotPerpArbitrage:
             # Iterate through the positions to check if they are open
             for position in data['assetPositions']:
                 if position.get('position')['coin'] == self.coin: 
+                    self.logger.info(f"Perp position open for {self.coin}.")
                     return True  # There is at least one open position on 'coin'
         return False  # No open positions found
     
@@ -182,7 +204,7 @@ class HypeSpotPerpArbitrage:
         if token_name in token_mark_price:
             return token_mark_price[token_name]
         else:
-            print(f"There is no mark price for {token_name}. We'll just return 0.0.")
+            self.logger.info(f"There is no mark price for {token_name}. We'll just return 0.0.")
             return 0.0
 
     def _get_token_markPx(self):
@@ -287,7 +309,7 @@ class HypeSpotPerpArbitrage:
             if "resting" in status:
                 oid = status["resting"]["oid"]
                 order_status = self.exchange.info.query_order_by_oid(self.wallet, oid)
-                print("Order status by oid:", order_status)
+                self.logger.info("Order status by oid:", order_status)
 
                 # Wait until filled
                 while True:
@@ -296,9 +318,9 @@ class HypeSpotPerpArbitrage:
                         break
 
                     if is_buy:
-                        print("Waiting for spot buy order to be filled.")
+                        self.logger.info("Waiting for spot buy order to be filled.")
                     else:
-                        print("Waiting for spot sell order to be filled.")
+                        self.logger.info("Waiting for spot sell order to be filled.")
 
         return self.spot_order_result
     
@@ -324,23 +346,6 @@ class HypeSpotPerpArbitrage:
         
     def place_perp_limit_order(self, size, price, is_buy=False):
         self.perp_order_result = self.exchange.order(self.coin, is_buy, size, price, {"limit": {"tif": "Gtc"}})
-
-        # print_json(self.perp_order_result)
-
-        # # Query the order status by oid
-        # if self.perp_order_result["status"] == "ok":
-        #     status = self.perp_order_result["response"]["data"]["statuses"][0]
-        #     if "resting" in status:
-        #         oid = status["resting"]["oid"]
-        #         order_status = self.info.query_order_by_oid(self.wallet, oid)
-        #         print("Order status by oid:", order_status)
-
-        #         # Wait until filled
-        #         while True:
-        #             order_status = self.exchange.info.query_order_by_oid(self.wallet, oid)
-        #             if order_status['order']['status'] == 'filled':
-        #                 break
-        #             print("Waiting for perp short order to be filled.")
         return self.perp_order_result   
 
     def place_perp_market_order(self, is_buy=False):
@@ -349,44 +354,44 @@ class HypeSpotPerpArbitrage:
         # price = self._perp_ask_price_at_level(1)
 
         if not size > 0:
-            print(f"No spot balance. Spot Buy May NOT SUCCEED.")
+            self.logger.info(f"No spot balance. Spot Buy May NOT SUCCEED.")
             return
         
         _, size = self._round_perp_px_sz(0.0, size)
 
-        print(f"There are {size} {self.coin} in the balance.")
-        print(f"We are going to open corresponding amount of short position.")
+        self.logger.info(f"There are {size} {self.coin} in the balance.")
+        self.logger.info(f"We are going to open corresponding amount of short position.")
 
         self.perp_order_result = self.exchange.market_open(self.coin, is_buy, size, slippage=self.slippage)
         if self.perp_order_result["status"] == "ok":
             for status in self.perp_order_result["response"]["data"]["statuses"]:
                 try:
                     filled = status["filled"]
-                    print(f'Order #{filled["oid"]} filled {filled["totalSz"]} @{filled["avgPx"]}')
+                    self.logger.info(f'Order #{filled["oid"]} filled {filled["totalSz"]} @{filled["avgPx"]}')
                 except KeyError:
-                    print(f'Error: {status["error"]}')        
+                    self.logger.info(f'Error: {status["error"]}')        
 
         return self.perp_order_result
 
     def close_positions(self):   
         # Sell all spot 
-        print(f"We try to sell all {self.coin}.")
+        self.logger.info(f"We try to sell all {self.coin}.")
         coin_spot_balance = self.get_spot_balance_by_token(self.coin)
         if coin_spot_balance > 0:
             self.place_spot_limit_order(is_buy=False)
         else:
-            print(f"No spot balance. Nothing to sell.")
+            self.logger.info(f"No spot balance. Nothing to sell.")
             
         # Close short perp
-        print(f"We try to close all {self.coin}.")
+        self.logger.info(f"Now we try to close all {self.coin}.")
         order_result = self.exchange.market_close(self.coin)
         if order_result["status"] == "ok":
             for status in order_result["response"]["data"]["statuses"]:
                 try:
                     filled = status["filled"]
-                    print(f'Order #{filled["oid"]} filled {filled["totalSz"]} @{filled["avgPx"]}')
+                    self.logger.info(f'Order #{filled["oid"]} filled {filled["totalSz"]} @{filled["avgPx"]}')
                 except KeyError:
-                    print(f'Error: {status["error"]}')
+                    self.logger.info(f'Error: {status["error"]}')
 
     def allocate_spot_perp_balance(self):
         """
@@ -400,26 +405,26 @@ class HypeSpotPerpArbitrage:
         total_usdc = balances['TOTAL']
         allocation = total_usdc / 2
 
-        print(f"The current usdc_spot is {usdc_spot} and usdc_perp is {usdc_perp}.")
+        self.logger.info(f"The current usdc_spot is {usdc_spot} and usdc_perp is {usdc_perp}.")
 
         # If usdc_perp > usdc_spot, transfer (usdc_perp - usdc_spot)/2 from perp to spot
         # If usdc_perp < usdc_spot, transfer (usdc_spot - usdc_perp)/2 from spot to perp
         if usdc_perp > usdc_spot:
             transfer_amount = (usdc_perp - usdc_spot) / 2
             transfer_result = self.exchange.usd_class_transfer(transfer_amount, False)
-            print("Since usdc_perp > usdc_spot, transfer from perp to spot: ", transfer_result)
+            self.logger.info("Since usdc_perp > usdc_spot, transfer from perp to spot: ", transfer_result)
         else:
             transfer_amount = (usdc_spot - usdc_perp) / 2
             transfer_result = self.exchange.usd_class_transfer(transfer_amount, True)
-            print("Since usdc_spot > usdc_perp, transfer from spot to perp: ", transfer_result)
+            self.logger.info("Since usdc_spot > usdc_perp, transfer from spot to perp: ", transfer_result)
         
         new_balances = self.get_usdc_balances()
         new_usdc_spot = new_balances['USDC_SPOT']
         new_usdc_perp = new_balances['USDC_PERP']
 
         if abs(new_usdc_perp - allocation) < 0.0001 and abs(new_usdc_spot - allocation) < 0.0001:
-            print(f"The usdc_spot is {new_usdc_spot} and the usdc_perp is {new_usdc_perp}")
-            print(f"Allocation complete and successful.")
+            self.logger.info(f"The usdc_spot is {new_usdc_spot} and the usdc_perp is {new_usdc_perp}")
+            self.logger.info(f"Allocation complete and successful.")
         
         return allocation
 
@@ -485,8 +490,8 @@ class HypeSpotPerpArbitrage:
             position_value = data['assetPositions'][0]['position']['positionValue']
             return float(position_value)  # Convert the position value to float
         except (KeyError, IndexError) as e:
-            print(f"Error extracting position_value: {e}")
-            print(f"Possibly because the system just closed positions. Please wait for 30 minutes.")
+            self.logger.info(f"Error extracting position_value: {e}")
+            self.logger.info(f"Possibly because the system just closed positions. Please wait for 30 minutes.")
             return None
 
     def check_funding_rate(self):
@@ -498,19 +503,20 @@ class HypeSpotPerpArbitrage:
 
                 # Only operate when the funding rate is positive
                 if funding_rate > 0:
-                    print(f"[{now}] Funding rate {funding_rate} is positive.")
+                    self.logger.info(f"[{now}] Funding rate {funding_rate} is positive.")
                     if not self.is_spot_open and not self.is_perp_open:
                         self.allocation = self.allocate_spot_perp_balance()
                         self.place_spot_limit_order(is_buy=True)
+                        self.is_spot_open = True
                         self.place_perp_market_order(is_buy=False)
                         self.is_perp_open = True
                         # self.initial_position_value = self.get_position_value()
                     else:
-                        print(f"Orders are already open.")
+                        self.logger.info(f"Orders are already open.")
                 
                 else:
+                    self.logger.info(f"[{now}] Funding rate is {funding_rate}, negative. We close positions.")
                     if self.is_spot_open and self.is_perp_open:
-                        print(f"[{now}] Funding rate is {funding_rate}, negative. We close positions.")
                         self.close_positions()
                         self.is_spot_open = False
                         self.is_perp_open = False
@@ -538,20 +544,19 @@ class HypeSpotPerpArbitrage:
 
                     # If the position value has fallen below 40% of the original value, close the positions
                     if current_position_value <= threshold:
-                        print(f"Position value fell by 40% (current: {current_position_value}, threshold: {threshold}). Closing positions.")
+                        self.logger.info(f"Position value fell by 40% (current: {current_position_value}, threshold: {threshold}). Closing positions.")
                         self.close_positions()
                         self.is_spot_open = False
                         self.is_perp_open = False
                     else:
-                        print(f"Position value is safe. Current: {current_position_value}, Threshold: {threshold}")
+                        self.logger.info(f"Position value is safe. Current: {current_position_value}, Threshold: {threshold}")
 
                 # Sleep for 5 minutes before checking the position value again
                 time.sleep(5 * 60)
 
             except Exception as e:
-                print(f"Position value check error: {e}")
+                self.logger.info(f"Position value check error: {e}")
                 time.sleep(60)
-
 
     def check_account_value(self):
         while True:
@@ -561,13 +566,13 @@ class HypeSpotPerpArbitrage:
                     relevant_values = self._extract_relevant_values(user_state)
                     self._check_and_warn(relevant_values)
                 else:
-                    print("Perps not open yet. Waiting for perps to open.")
+                    self.logger.info("Perps not open yet. Waiting for perps to open.")
 
                 # Sleep for 5 minutes before checking the account value again
                 time.sleep(5 * 60)
 
             except Exception as e:
-                print(f"Account value check error: {e}")
+                self.logger.info(f"Account value check error: {e}")
                 time.sleep(60)
 
     def _extract_relevant_values(self, data):
@@ -668,22 +673,22 @@ class HypeSpotPerpArbitrage:
         # Get the current timestamp
         current_time = self._curr_timestamp()
 
-        print(f"[{current_time}]Checking account status...")
-        print(f"Account Value: {account_value}")
-        print(f"Cross Maintenance Margin Used: {maintenance_margin}")
-        print(f"Warning Threshold: {warning_threshold}")
-        print(f"Liquidation Price: {liquidation_price}")
-        print(f"Mark Price: {mark_price}")
+        self.logger.info(f"[{current_time}]Checking account status...")
+        self.logger.info(f"Account Value: {account_value}")
+        self.logger.info(f"Cross Maintenance Margin Used: {maintenance_margin}")
+        self.logger.info(f"Warning Threshold: {warning_threshold}")
+        self.logger.info(f"Liquidation Price: {liquidation_price}")
+        self.logger.info(f"Mark Price: {mark_price}")
         
         # Check if account value is close to or below the threshold
         if account_value <= warning_threshold:
-            print(f"[{current_time}]⚠️ Warning: Account value is close to the maintenance margin threshold.")
-            print("Consider reducing your position to avoid liquidation!")
+            self.logger.info(f"[{current_time}]⚠️ Warning: Account value is close to the maintenance margin threshold.")
+            self.logger.info("Consider reducing your position to avoid liquidation!")
         elif mark_price >= liquidation_price:
-            print(f"[{current_time}]⚠️ Warning: The current mark price is close to the liquidation price!")
-            print("Consider taking action to avoid liquidation!")
+            self.logger.info(f"[{current_time}]⚠️ Warning: The current mark price is close to the liquidation price!")
+            self.logger.info("Consider taking action to avoid liquidation!")
         else:
-            print(f"[{current_time}]✅ Your account is safe for now.\n")
+            self.logger.info(f"[{current_time}]✅ Your account is safe for now.\n")
 
     def _curr_timestamp(self):
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -703,4 +708,5 @@ class HypeSpotPerpArbitrage:
 
 if __name__ == "__main__":
     arbitrage = HypeSpotPerpArbitrage("HYPE")
+    # arbitrage.close_positions()
     arbitrage.run_strategy()
